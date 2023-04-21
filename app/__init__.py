290385f1afbb471d909209b61146773e5cfe024d
Pratham Kamath <rohithkamath2001@gmail.com>
2023-04-21 12:39:05 +0530

    second changes done

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import psycopg2
 
 app = Flask(__name__)
 postgres = psycopg2.connect(
-        host="@dpg-ch135533cv203buiagig-a.oregon-postgres.render.com",
+        host="dpg-ch135533cv203buiagig-a.oregon-postgres.render.com",
         database="todo_app_k9n3",
         user="todo_app_k9n3_user",
         password="Ry3t3cg1HL7veNeTVoYu8U0zJG8CVDGR")
